70cfd7f00ca0535677ca946f0195658368f2dbf5
Yasar Kucukefe <yasar.kucukefe@gmail.com>
2024-03-27 12:09:47 +0300

    Hafta 7

---
+++ hafta-7/fetch.js
@@ -0,0 +1,20 @@
+
+const received_json = (json) => {
+	console.log(json);
+
+	document.querySelector("#urun_ad").innerHTML = json.title;
+	document.querySelector("#urun_desc").innerHTML = json.description;
+
+	const div_images = document.querySelector("#images");
+
+	json.images.forEach(img_src => {
+		const img = `<img src='${img_src}'>`;
+		div_images.innerHTML += img;
+	})
+}
+
+
+fetch('https://dummyjson.com/products/1')
+.then(res => res.json())
+.then(json => received_json(json))
+
